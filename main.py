b8a64ac8e9c1125191524c5b6d7a9d876df7d929
slemonade <lemon.thanakrit@gmail.com>
2022-06-20 15:45:30 +0700

    fin

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import requests
-import urllib.request
 
-class AppURLopener(urllib.request.FancyURLopener):
-    version = "Mozilla/5.0"
+header = {
+    'user-agent': 'Mozilla/5.0'
+}
 
-opener = AppURLopener()
 
 def convert_int_to_str(value, target):
     val_10 = 0
@@ -21,57 +20,15 @@ def main():
     file = open(name, "r")
     filetype = ['.png', '.jpg', '.jpeg']
 
-    
-
     for line in file:
         for each in filetype:
             if each in line:
-                
-                # response = requests.get(line, stream=True).content
-                # print(response)
-                # img_name = url + convert_int_to_str(count, 5) + each
+                response = requests.get(line[:-1], headers=header)
                 img_name = url + str(count) + each
                 print(line)
                 print(img_name)
-                # with open(img_name, 'wb') as handler:
-                #     handler.write(response)
-                urllib.request.urlretrieve(line, img_name)
+                with open(img_name, 'wb') as handler:
+                    handler.write(response.content)
                 count += 1
-        if count > 5:
-            break
-
-
-    # for line in file:
-    #     for each in filetype:
-    #         if each in line:
-                
-    #             response = requests.get(line, stream=True).content
-    #             # print(response)
-    #             # img_name = url + convert_int_to_str(count, 5) + each
-    #             img_name = url + str(count) + each
-    #             print(line)
-    #             print(img_name)
-    #             # with open(img_name, 'wb') as handler:
-    #             #     handler.write(response)
-    #             img = open(img_name, "wb")
-    #             img.write(response)
-    #             img.close()
-    #             count += 1
-    #     if count > 5:
-    #         break
-
-    # for line in file:
-    #     for each in filetype:
-    #         if each in line:
-    #             print(line)
-    #             response = requests.get(line, stream=True).content
-    #             img_name = url + convert_int_to_str(count, 5) + each
-    #             # with open(img_name, 'wb') as handler:
-    #             #     handler.write(response)
-    #             img = open(img_name, "wb")
-    #             img.write(response)
-    #             img.close()
-    #             count += 1
-
 
 main()
